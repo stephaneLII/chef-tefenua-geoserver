c05af8a4f318f7b801be8e3f49f6d5cc1ef7d42e
stephaneLII <stephane.lii@informatique.gov.pf>
2015-03-13 11:42:55 -1000

    Ajout des jars, des fonts.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,8 @@
 default['chef-tefenua-geoserver']['geoserver_svn_link'] = 'http://server/svn/project'
+default['chef-tefenua-geoserver']['jar_svn_link'] = 'http://server/svn/jar'
+default['chef-tefenua-geoserver']['jar_dir'] = '/opt/tefenua_jars'
+default['chef-tefenua-geoserver']['fonts_svn_link'] = 'http://server/svn/fonts'
+default['chef-tefenua-geoserver']['fonts_dir'] = '/usr/share/fonts'
 default['chef-tefenua-geoserver']['user_svn'] = 'user1'
 default['chef-tefenua-geoserver']['passwd_svn'] = 'password'
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -18,6 +18,67 @@ package 'subversion' do
    action :install
 end
 
+package 'fontconfig' do
+   action :install
+end
+
+delete_dir_fonts = "rm -rf #{node['chef-tefenua-geoserver']['fonts_dir']}" + "/*"
+delete2_dir_fonts = "rm -rf #{node['chef-tefenua-geoserver']['fonts_dir']}" + "/.svn"
+
+bash 'delete-fonts' do
+  user 'root'
+  code <<-EOH
+   #{delete_dir_fonts}
+   #{delete2_dir_fonts}
+  EOH
+end
+
+subversion "fonts" do
+  repository "#{node['chef-tefenua-geoserver']['fonts_svn_link']}"
+  revision "HEAD"
+  destination "#{node['chef-tefenua-geoserver']['fonts_dir']}"
+  svn_username "#{node['chef-tefenua-geoserver']['user_svn']}"
+  svn_password "#{node['chef-tefenua-geoserver']['passwd_svn']}"
+  action :sync
+end
+
+bash 'configure-fonts' do
+  user 'root'
+  code <<-EOH
+   fc-cache -f
+  EOH
+end
+
+# Installing specific JARS in the jvm
+#####################################
+
+directory node['chef-tefenua-geoserver']['jar_dir'] do
+   owner "root"
+   group "root"
+   mode '0755'
+   action :create
+   recursive true
+end
+
+subversion "jar" do
+  repository "#{node['chef-tefenua-geoserver']['jar_svn_link']}"
+  revision "HEAD"
+  destination "#{node['chef-tefenua-geoserver']['jar_dir']}"
+  svn_username "#{node['chef-tefenua-geoserver']['user_svn']}"
+  svn_password "#{node['chef-tefenua-geoserver']['passwd_svn']}"
+  action :sync
+end
+
+extract_jars = "ls #{node['chef-tefenua-geoserver']['jar_dir']}/*.tgz | while read f ; do tar -xzf $f ; done"
+
+bash 'extract_jar' do
+  user 'root'
+  cwd "#{node['java']['java_home']}"
+  code <<-EOH
+   #{extract_jars}
+  EOH
+end
+
 directory node['tomcat']['webapp_dir'] + '/geoserver' do
    owner "#{node['tomcat']['user']}"
    group "#{node['tomcat']['group']}"
